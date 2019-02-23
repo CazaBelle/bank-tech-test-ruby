0decae4466d98fbc3b82ffc8d281cdb52689c285
Carol Gardiner <carolan.gardiner@gmail.com>
2019-02-23 19:59:25 +0000

    add feature to allow user to deposit

--- lib/account.rb
+++ lib/account.rb
@@ -1,2 +1,10 @@
 class Account 
+  attr_reader :balance
+  def initialize
+    @balance = 0 
+  end
+
+  def deposit(amount)
+    @balance = @balance + amount 
+  end 
 end 
--- spec/features/user_stories_features_spec.rb
+++ spec/features/user_stories_features_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe 'User Stories' do
     expect(account).to be_a Account
   end 
 
+  describe '#deposit' do 
+    it 'allows user to make a deposit' do 
+      account = Account.new 
+      account.deposit(1000)
+      expect(account.balance).to eq 1000
+    end 
+  end 
 end 
